f32ff6cf774bc7a3555ee9784942c1bb84fbc383
piopoi <kangsw1988@gmail.com>
2025-02-05 21:53:24 +0900

    feat: html 작성 코드 추가 (ch1.7)

--- src/main/java/refactoring/ch1/InvoiceGenerator.java
+++ src/main/java/refactoring/ch1/InvoiceGenerator.java
@@ -14,6 +14,10 @@ public class InvoiceGenerator {
         return renderPlainText(createStatementData(invoice, plays));
     }
 
+    public String htmlStatement(Invoice invoice, Map<String, Play> plays) {
+        return renderHtml(createStatementData(invoice, plays));
+    }
+
     private StatementData createStatementData(Invoice invoice, Map<String, Play> plays) {
         StatementData statementData = new StatementData();
         statementData.setCustomer(invoice.customer());
@@ -47,6 +51,24 @@ public class InvoiceGenerator {
         }
         result.append(String.format("총액: %s\n", usd(totalAmount(data))));
         result.append(String.format("적립 포인트: %d점\n", totalVolumeCredits(data)));
+        return result.toString();
+    }
+
+    private String renderHtml(StatementData data) {
+        StringBuilder result = new StringBuilder();
+        result.append("<h1>청구 내역 (고객명: " + data.getCustomer() + ")</h1>\\n");
+        result.append("<table>\\n");
+        result.append(" <tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>");
+        for (Performance performance : data.getPerformances()) {
+            result.append(" <tr>\\n");
+            result.append("  <td>" + performance.getPlay().name() + "</td>\\n");
+            result.append("  <td>(" + performance.getAudience() + "석)</td>\\n");
+            result.append("  <td>" + usd(performance.getAmount()) + "</td>\\n");
+            result.append(" </tr>\\n");
+        }
+        result.append("</table>\\n");
+        result.append("<p>총액: <em>" + usd(totalAmount(data)) + "</em></p>\\n");
+        result.append("<p>적립 포인트: <em>" + totalVolumeCredits(data) + "}</em>점</p>\\n");
         return result.toString();
     }
 
